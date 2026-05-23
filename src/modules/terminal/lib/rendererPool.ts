f8fcaef49ef9e42470b1acb6056f56bea436d099
nihal <nihalsheikh585@gmail.com>
2026-05-23 13:10:00 +0530

    fix(terminal): Add Copy and Paste keyboard shortcuts

--- src/modules/terminal/lib/rendererPool.ts
+++ src/modules/terminal/lib/rendererPool.ts
@@ -714,8 +714,12 @@ const IS_MAC =
   /Mac|iPhone|iPad/.test(navigator.userAgent);
 
 function isTerminalCopy(e: KeyboardEvent): boolean {
+  if (IS_MAC) {
+    return (
+      e.metaKey && !e.ctrlKey && !e.shiftKey && !e.altKey && e.code === "KeyC"
+    );
+  }
   return (
-    !IS_MAC &&
     e.ctrlKey &&
     e.shiftKey &&
     !e.altKey &&
@@ -725,8 +729,12 @@ function isTerminalCopy(e: KeyboardEvent): boolean {
 }
 
 function isTerminalPaste(e: KeyboardEvent): boolean {
+  if (IS_MAC) {
+    return (
+      e.metaKey && !e.ctrlKey && !e.shiftKey && !e.altKey && e.code === "KeyV"
+    );
+  }
   return (
-    !IS_MAC &&
     e.ctrlKey &&
     e.shiftKey &&
     !e.altKey &&
